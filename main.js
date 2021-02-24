bc360eb7fe60e4fb41af78df7353be0b1bad2b05
yoel7 <1613195@gmail.com>
2021-02-24 20:27:04 +0200

    11

--- main.js
+++ main.js
@@ -2,19 +2,36 @@
 // a1= document.querySelector('.a,b1');console.log(a1);
 arr=[];
 switchB=false;
-arrBool=[];
+// arrBool=[];
 function crGame(p){
     nam=p;
     p=p*2;
     for(let a=p;a>0;a--){
         arr[a-1]=[];
-        arrBool[a-1]=[];
+        // arrBool[a-1]=[];
+    }
+    for(let a=p*nam;a>0;a--){
+        let randNumImg = Math.floor(Math.random() * 10);
+        for (let i=2;i>0;i--){
+            for (let j=0;j<50;){
+               var randNum1= Math.floor(Math.random() * p);
+                var randNum2=Math.floor(Math.random() * p);
+                if (!arr[randNum1][randNum2]){
+                    arr[randNum1][randNum2]=randNumImg+1;  j=50; 
+                }
+            // if (randNum1)
+            }
+        }
+    }
+    for(let a=p;a>0;a--){
         document.querySelector('#game').innerHTML+=`<div id="a${a}"></div>`;
         for(let b=p;b>0;b--){
-           arr[a-1][b-1]=`a=${a},b=${b}`
+        //    arr[a-1][b-1]=`a=${a},b=${b}`
+        //    arr[a-1][b-1]=5
+           numImg=arr[a-1][b-1]
             arrBool[a-1][b-1]=false;
             document.querySelector("#a"+a+"").innerHTML+=`
-            <img  id="a${a}${b}img" onclick="click1('a${a}${b}')" style="display: inline-block; width: 50px;height: 50px;" src='images/${b}.png'><//img>
+            <img  id="a${a}${b}img" onclick="click1('a${a}${b}')" style="display: inline-block; width: 50px;height: 50px;" src='images/${numImg}.png'><//img>
             <img  id="a${a}${b}b" onclick="click1('a${a}${b}')" style="display: none; width: 50px;height: 50px;" src='images/blank.png'><//img>
             `;
         }
@@ -41,5 +58,17 @@ function click1(e){
     }
 
 }
-
+// function mamare(rNI){
+//     // debugger
+//     // for (let i=2;i>0;i--){
+//     //     for (let j=0;j<50;){
+//     //        var randNum1= Math.floor(Math.random() * p);
+//     //         var randNum2=Math.floor(Math.random() * p);
+//     //         if (!arr[randNum1][randNum2]){
+//     //             arr[randNum1][randNum2]=rNI+1;  j=50; 
+//     //         }
+//     //     // if (randNum1)
+//     //     }
+//     // }
+// }
 crGame(2)
